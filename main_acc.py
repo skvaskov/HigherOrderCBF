8be135ca82f5a605cc2a98648def96108647784a
Sean Vaskov <skvaskov@umich.edu>
2022-02-27 20:12:46 -0500

    update adaptive cruise control example with lyapunov stability

--- main_acc.py
+++ main_acc.py
@@ -1,52 +1,83 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from utils import rk4
+import osqp
+from scipy import sparse
 
 #parameters for lead and ego vehicle
-c1 = 0.001
-c2 = 0.01
-c3 = 0.2
+c0 = 0.1
+c1 = 5
+c2 = 0.25
 
-m_ego = 2000
-m_lead = 2000
+m_ego = 1650
+m_lead = 1650
 
-mu = 0.7
+ca = 0.4
+cd = 0.4
 g = 9.81
 
 #dynamics for lead and ego vehicles
 def f(t,x,u):
     u_ego = u[0]
-    u_lead = u[1]
+    v_lead = u[1]
 
     x_ego = x[0]
     v_ego = x[1]
     x_lead = x[2]
-    v_lead = x[3]
+    #v_lead = x[3]
 
-    Fr_ego = c1 * np.sign(v_ego) + c2 * v_ego + c3 * v_ego ** 2
-    Fr_lead = c1 * np.sign(v_lead) + c2 * v_lead + c3 * v_lead ** 2
-
+    Fr_ego = c0 * np.sign(v_ego) + c1 * v_ego + c2 * v_ego ** 2
+    #Fr_lead = c1 * np.sign(v_lead) + c2 * v_lead + c3 * v_lead ** 2
     return np.array([v_ego,
             1/m_ego * (-Fr_ego + u_ego),
-            v_lead,
-            1 / m_lead * (-Fr_lead + u_lead)])
+            v_lead])
 
+def get_state_dependent_matrices_for_qp(x,v_des,epsilon):
+    v_ego = x[1]
+    Fr_ego = c0 * np.sign(v_ego) + c1 * v_ego + c2 * v_ego ** 2
+    q = np.array([-2 * Fr_ego / m_ego ** 2, 0])
+
+    #lyapunov control
+    LgVacc = 2*(v_ego-v_des) / m_ego
+    LfVacc = -2*(v_ego-v_des) / m_ego * Fr_ego
+    Vacc = (v_ego-v_des)**2
+
+    A = sparse.csc_matrix([[1,0],[LgVacc, -1]])
+    u = np.array([ca*m_ego*g,-LfVacc-epsilon*Vacc])
+    return q, A, u
+
+#setup OSQP
+pacc = 1
+prob = osqp.OSQP()
+P = sparse.csc_matrix([[2/m_ego**2,0],[0,2*pacc]])
+l = np.concatenate(([-cd*m_ego*g],-np.inf * np.ones(1)))
+x0 = [0,20,100]
+v_d = 24
+epsilon = 10
+q,A,u = get_state_dependent_matrices_for_qp(x0,v_d,epsilon)
+
+# Setup workspace
+prob.setup(P, q, A, l, u, warm_start=True)
 
 #simulation parameters
 T = 30
 dt = 0.1
-
+v_lead = 13.89
 tvec = np.arange(0,T+dt,dt)
 N = len(tvec)
-X = np.zeros((N,4))
-X[0,:] = [0,25,50,25]
+X = np.zeros((N,3))
+X[0,:] = x0
 U = np.zeros((N-1,2))
 
 for i in range(len(tvec)-1):
-    u_ego = np.minimum(np.maximum(-m_ego/5*(X[i, 1] - 30), -mu * m_ego * g), mu*m_ego*g)
-    u_lead = np.minimum(np.maximum(-m_lead/5*(X[i, 3] - 20), -mu * m_lead * g), mu * m_lead * g)
+    q_new, A_new, u_new = get_state_dependent_matrices_for_qp(X[i,:], v_d, epsilon)
+    prob.update(q=q_new, u=u_new)
+    prob.update(Ax=A_new.data)
 
-    U[i,:] = [u_ego,u_lead]
+    res = prob.solve()
+    u_ego = res.x[0]
+
+    U[i,:] = [u_ego,v_lead]
 
     X[i+1,:] = rk4(f,tvec[i],X[i,:],U[i,:],dt)
 
@@ -57,13 +88,11 @@ ax[0].plot(tvec,X[:,2]-X[:,0])
 ax[0].set_ylabel('xlead-xego [m]')
 
 ax[1].plot(tvec,X[:,1])
-ax[1].plot(tvec,X[:,3])
 ax[1].set_ylabel('v [m/s]')
 
 ax[2].plot(tvec[:-1],U[:,0]/m_ego)
-ax[2].plot(tvec[:-1],U[:,1]/m_lead)
-ax[2].plot(tvec,mu*g*np.ones(N),'r--')
-ax[2].plot(tvec,-mu*g*np.ones(N),'r--')
+ax[2].plot(tvec,ca*g*np.ones(N),'r--')
+ax[2].plot(tvec,-cd*g*np.ones(N),'r--')
 ax[2].set_xlabel('t [s]')
 ax[2].set_ylabel('u/m [m/s^2]')
 
