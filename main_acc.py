ab8f0072696f998fb79393f601014854b81c2f10
Sean Vaskov <skvaskov@umich.edu>
2022-03-01 00:43:50 -0500

    update with barrier function (Form 2 in paper)

--- main_acc.py
+++ main_acc.py
@@ -20,6 +20,14 @@ g = 9.81
 v_max = 30.0
 v_min = 0.0
 
+v_lead = 13.89
+v_des = 24
+
+#parameters for HOCBF
+epsilon = 10.0
+p = 1
+delta = 10.0
+
 #dynamics for lead and ego vehicles
 def f(t,x,u):
     u_ego = u[0]
@@ -34,9 +42,11 @@ def f(t,x,u):
             1/m_ego * (-Fr_ego + u_ego),
             v_lead])
 
-def get_state_dependent_matrices_for_qp(x,v_des,epsilon):
+def get_state_dependent_matrices_for_qp(x):
     #note that we need to change this method to update only the data in A
     #not create a new sparse csc object in order to use OSQP's update function
+    x_lead = copy.copy(x[2])
+    x_ego = copy.copy(x[0])
     v_ego = copy.copy(x[1])
     Fr_ego = c0 * np.sign(v_ego) + c1 * v_ego + c2 * v_ego ** 2
     q = np.array([-2 * Fr_ego / m_ego ** 2,0])
@@ -46,31 +56,36 @@ def get_state_dependent_matrices_for_qp(x,v_des,epsilon):
     mLfVacc = -2*(v_ego-v_des) * Fr_ego
     Vacc = (v_ego-v_des)**2
 
+    #safety barrier
+    bdot = v_lead-v_ego
+    b = x_lead-x_ego-delta
+
 
     A = sparse.csc_matrix([[1,0],[0,1],
                            [mLgVacc,-m_ego],
                            [1,0],
-                           [-1,0]])
+                           [-1,0],
+                           [1,0]])
     u = np.array([ca*m_ego*g,np.inf,
                   -mLfVacc-m_ego*epsilon*Vacc,
                   Fr_ego+m_ego*(v_max-v_ego),
-                  -Fr_ego+m_ego*(v_ego-v_min)])
+                  -Fr_ego+m_ego*(v_ego-v_min),
+                  Fr_ego+m_ego*(2*p*bdot+p**2*b)])
     return q, A, u
 
 #setup OSQP
-pacc = 1
+pacc = 1.0
 prob = osqp.OSQP()
 P = sparse.csc_matrix([[2/m_ego**2,0],[0,2*pacc]])
 
-l = np.concatenate((np.array([-cd*m_ego*g,0]),-np.inf * np.ones(3)))
+l = np.concatenate((np.array([-cd*m_ego*g,0]),-np.inf * np.ones(4)))
 x0 = [0.0,20.0,100.0]
-v_d = 20.0
-epsilon = 10.0
+
 
 #simulation parameters
 T = 30
 dt = 0.1
-v_lead = 13.89
+
 tvec = np.arange(0,T+dt,dt)
 N = len(tvec)
 X = np.zeros((N,3))
@@ -81,7 +96,7 @@ for i in range(len(tvec)-1):
     #note that we need to change the below method to update only the data in A
     #not create a new sparse csc object in order to use OSQP's update function
 
-    q_new, A_new, u_new = get_state_dependent_matrices_for_qp(X[i,:], v_d, epsilon)
+    q_new, A_new, u_new = get_state_dependent_matrices_for_qp(X[i,:])
 
     prob = osqp.OSQP()
     prob.setup(P, q_new, A_new, l, u_new)
@@ -96,26 +111,35 @@ for i in range(len(tvec)-1):
 
 
 #plot results
-fig,ax = plt.subplots(nrows=3,ncols=1)
+fig,ax = plt.subplots(nrows=2,ncols=2)
 
-ax[0].plot(tvec,X[:,2]-X[:,0])
-ax[0].set_ylabel('xlead-xego [m]')
+ax[0,0].plot(tvec,X[:,2]-X[:,0])
+ax[0,0].set_ylabel('xlead-xego [m]')
+ax[0,0].plot(tvec,delta*np.ones(N),'r--')
 
-ax[1].plot(tvec,X[:,1])
-ax[1].set_ylabel('v [m/s]')
+ax[1,0].plot(tvec,v_lead-X[:,1]+p*(X[:,2]-X[:,0]-delta))
+ax[1,0].set_ylabel('$\psi$(x(t))$_1$')
+ax[1,0].plot(tvec,delta*np.zeros(N),'r--')
 
-ax[2].plot(tvec[:-1],U[:,0]/m_ego)
-ax[2].plot(tvec,ca*g*np.ones(N),'r--')
-ax[2].plot(tvec,-cd*g*np.ones(N),'r--')
-ax[2].set_xlabel('t [s]')
-ax[2].set_ylabel('u/m [m/s^2]')
+ax[0,1].plot(tvec,X[:,1])
+ax[0,1].set_ylabel('v [m/s]')
+ax[0,1].plot(tvec,v_max*np.ones(N),'r--')
+ax[0,1].plot(tvec,v_min*np.ones(N),'r--')
+
+
+ax[1,1].plot(tvec[:-1],U[:,0]/m_ego)
+ax[1,1].plot(tvec,ca*g*np.ones(N),'r--')
+ax[1,1].plot(tvec,-cd*g*np.ones(N),'r--')
+ax[1,1].set_xlabel('t [s]')
+ax[1,1].set_ylabel('u/m [m/s^2]')
 
 fig.tight_layout()
 
 fig2,ax2 = plt.subplots(1)
 ax2.plot(delta_acc)
 ax2.set_xlabel('t [s]')
-ax2.set_ylabel('slack variable')
+ax2.set_ylabel('$\delta$ acc')
+ax2.set_title('Slack variable for CLF')
 fig2.tight_layout()
 plt.show()
 
